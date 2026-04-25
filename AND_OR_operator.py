a0fd754fb9833640b04853e133582a87daad4013
savargb <savargb123@gmail.com>
2026-04-25 12:30:56 +0545

    v1

---
+++ AND_OR_operator.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All boolean numbers have the value as 'True' ")
+else:
+    print("Atleast one boolean number has the value as 'False'")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than zero")
+else:
+    print("No number is greater than zero")
+
+if b > 0 or c > 0:
+    print("Either of the number is greater than zero")
+else:
+    print("None of the numbers are greater than zero")
+
+
